3a0a244527089b5d838292a7581a732ad620487a
Ay-iman <ayomiderabiu00@gmail.com>
2023-10-15 09:38:33 +0100

    alx

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Alx frontend for fun
+# Alx frontend    
--- markdown2html.py
+++ markdown2html.py
@@ -16,34 +16,39 @@ Example:
 import argparse
 import pathlib
 import re
-
+import sys
+import html  # Added for HTML escaping
 
 def convert_md_to_html(input_file, output_file):
     '''
     Converts markdown file to HTML file
     '''
-    # Read the contents of the input file
-    with open(input_file, encoding='utf-8') as f:
-        md_content = f.readlines()
+    try:
+        # Read the contents of the input file
+        with open(input_file, encoding='utf-8') as f:
+            md_content = f.readlines()
 
-    html_content = []
-    for line in md_content:
-        # Check if the line is a heading
-        match = re.match(r'(#){1,6} (.*)', line)
-        if match:
-            # Get the level of the heading
-            h_level = len(match.group(1))
-            # Get the content of the heading
-            h_content = match.group(2)
-            # Append the HTML equivalent of the heading
-            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
-        else:
-            html_content.append(line)
+        html_content = []
+        for line in md_content:
+            # Check if the line is a heading
+            match = re.match(r'(#){1,6} (.*)', line)
+            if match:
+                # Get the level of the heading
+                h_level = len(match.group(1))
+                # Get the content of the heading and HTML escape it
+                h_content = html.escape(match.group(2))
+                # Append the HTML equivalent of the heading
+                html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
+            else:
+                html_content.append(html.escape(line))  # HTML escape content
 
-    # Write the HTML content to the output file
-    with open(output_file, 'w', encoding='utf-8') as f:
-        f.writelines(html_content)
+        # Write the HTML content to the output file
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.writelines(html_content)
 
+    except Exception as e:
+        print(f'An error occurred: {e}', file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
     # Parse command-line arguments
@@ -60,5 +65,3 @@ if __name__ == '__main__':
 
     # Convert the markdown file to HTML
     convert_md_to_html(args.input_file, args.output_file)
-
-
